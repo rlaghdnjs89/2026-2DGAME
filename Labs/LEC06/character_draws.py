5fbd437d97222049672f33f1a5e5ed7c7c55396f
rlaghdnjs89 <rlaghdnjs89@gmail.com>
2026-09-26 03:00:30 +0900

    삼각형 세 구간 순서대로 실행

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -49,7 +49,11 @@ def draw_boy(x,y):
 
 open_canvas(800,600)
 boy=load_image('character.png')
+
+move_AB()
+move_BC()
 move_CA()
+
 def move_circle():
     for degree in range(360):
         theta=math.radians(degree)
