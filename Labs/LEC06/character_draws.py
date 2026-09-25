fd86c7f3fe731f0ac26bbb4cac02fe9f3fc2053f
rlaghdnjs89 <rlaghdnjs89@gmail.com>
2026-09-26 01:34:01 +0900

    캐릭터 이미지 로드 및 화면 출력 추가

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,7 +1,13 @@
 from pico2d import *
 
 open_canvas(800,600)
+boy=load_image('character.png')
 
+clear_canvas()
+boy.draw(400,300)
+update_canvas()
+delay(1)
+close_canvas()
 def move_circle():
        print('circle')
 
