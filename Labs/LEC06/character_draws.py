d7ac984ddcbaac8ffc9aa1b2b6cfa0e940c1ccd9
rlaghdnjs89 <rlaghdnjs89@gmail.com>
2026-09-26 01:13:42 +0900

    세 운동 함수에 출력문 추가

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,10 +1,10 @@
 def move_circle():
-       pass
+       print('circle')
 
 def move_rectangle():
-       pass
+       print('rectangle')
 def move_triangle():
-       pass
+       print('triangle')
  
 while True:
       move_circle()
