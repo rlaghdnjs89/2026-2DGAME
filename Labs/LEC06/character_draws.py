736db03f2b72742e165fec33cbd53a4cdc272c8e
rlaghdnjs89 <rlaghdnjs89@gmail.com>
2026-09-26 02:56:38 +0900

    삼각형 BC 구간 이동 구현 및 확인용 호출 변경

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -26,7 +26,12 @@ def move_AB():
         draw_boy(x,y)
 
 def move_BC():
-       pass
+       n=100
+       for step in range(n+1):
+           t=step/n
+           x=700-300*t
+           y=100+400*t
+           draw_boy(x,y)
 
 def move_CA():
        pass
@@ -39,7 +44,7 @@ def draw_boy(x,y):
 
 open_canvas(800,600)
 boy=load_image('character.png')
-move_AB()
+move_BC()
 def move_circle():
     for degree in range(360):
         theta=math.radians(degree)
