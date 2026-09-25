4b2c5fb9f56a28d8ea0fc39c2325c6f2dd7a5d7d
rlaghdnjs89 <rlaghdnjs89@gmail.com>
2026-09-26 03:01:15 +0900

    삼각형 세 구간의 이동 단계 수를 50으로 조정

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -18,7 +18,7 @@ def move_left():
          draw_boy(50,y)
 
 def move_AB():
-    n=120
+    n=50
     for step in range(n+1):
         t=step/n 
         x=100+600*t
@@ -26,7 +26,7 @@ def move_AB():
         draw_boy(x,y)
 
 def move_BC():
-       n=100
+       n=50
        for step in range(n+1):
            t=step/n
            x=700-300*t
@@ -34,7 +34,7 @@ def move_BC():
            draw_boy(x,y)
 
 def move_CA():
-       n=100
+       n=50
        for step in range(n + 1):
         t = step / n
         x = 400 - 300 * t
